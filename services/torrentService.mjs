5ccd8e7e7699a18e8b096d11c260c0f3bf27b814
Gustavo Marques <gustavomarques.gml@gmail.com>
2023-10-01 12:41:12 -0300

    Addition of file torrent manager

--- services/torrentService.mjs
+++ services/torrentService.mjs
@@ -1,53 +1,116 @@
 import WebTorrent from "webtorrent";
 import { readFileSync, writeFileSync, existsSync } from "fs";
+import path from "path";
 
-export class TorrentService {
-  constructor(downloadPath, magnetUriFile = "magnetUris.json") {
-    this.client = new WebTorrent();
-    this.downloadPath = downloadPath;
-    this.magnetUriFile = magnetUriFile;
-    this.loadTorrents();
+class MagnetURIFileManager {
+  constructor(magnetURIFile = "magnetURIs.json") {
+    this.magnetURIFile = magnetURIFile;
   }
 
-  addTorrent(magnetUri) {
-    this.client.add(magnetUri, { path: this.downloadPath });
-
-    let magnetUris = [];
-    if (existsSync(this.magnetUriFile)) {
-      magnetUris = JSON.parse(readFileSync(this.magnetUriFile, "utf-8"));
-    }
-
-    magnetUris.push(magnetUri);
-    writeFileSync(this.magnetUriFile, JSON.stringify(magnetUris, null, 2));
+  extractInfoHash(magnetUri) {
+    const match = magnetUri.match(/xt=urn:btih:([a-fA-F0-9]+)/);
+    return match ? match[1].toLowerCase() : null;
   }
 
-  loadTorrents() {
-    if (existsSync(this.magnetUriFile)) {
-      const magnetUris = JSON.parse(readFileSync(this.magnetUriFile, "utf-8"));
-      magnetUris.forEach((magnetUri) => this.addTorrent(magnetUri));
+  magnetsAreEqual(magnetUri1, magnetUri2) {
+    const infoHash1 = this.extractInfoHash(magnetUri1);
+    const infoHash2 = this.extractInfoHash(magnetUri2);
+    return infoHash1 === infoHash2;
+  }
+
+  getMagnetURIs() {
+    let magnetURIs = [];
+    if (existsSync(this.magnetURIFile)) {
+      magnetURIs = JSON.parse(readFileSync(this.magnetURIFile, "utf-8"));
     }
+    return magnetURIs;
+  }
+
+  setMagnetURIs(magnetURIs) {
+    writeFileSync(this.magnetURIFile, JSON.stringify(magnetURIs, null, 2));
+  }
+
+  torrentExists(magnetURI, downloadPath) {
+    const magnetURIs = this.getMagnetURIs();
+
+    return magnetURIs.some(
+      ({ magnetURI: currentMagnetUri, downloadPath: currentDownloadPath }) =>
+        this.magnetsAreEqual(magnetURI, currentMagnetUri) &&
+        currentDownloadPath === downloadPath
+    );
+  }
+
+  addTorrentOnFile(magnetURI, downloadPath = "") {
+    const magnetURIs = this.getMagnetURIs();
+
+    const torrentAlredyExists = magnetURIs.some(
+      ({ magnetURI: currentMagnetUri, downloadPath: currentDownloadPath }) =>
+        this.magnetsAreEqual(magnetURI, currentMagnetUri) &&
+        currentDownloadPath === downloadPath
+    );
+
+    if (torrentAlredyExists) {
+      return;
+    }
+
+    magnetURIs.push({
+      magnetURI: magnetURI,
+      downloadPath: downloadPath,
+    });
+
+    this.setMagnetURIs(magnetURIs);
   }
 
   removeTorrentFromFile(magnetURI) {
-    if (existsSync(this.magnetUriFile)) {
-      const magnetUris = JSON.parse(readFileSync(this.magnetUriFile, "utf-8"));
-      const torrentIndex = magnetUris.findIndex((uri) => magnetURI === uri);
+    if (existsSync(this.magnetURIFile)) {
+      const magnetURIs = JSON.parse(readFileSync(this.magnetURIFile, "utf-8"));
+      const torrentIndex = magnetURIs.findIndex(
+        ({ magnetURI: currentMagnetUri }) =>
+          this.magnetsAreEqual(magnetURI, currentMagnetUri)
+      );
 
       if (torrentIndex > -1) {
-        magnetUris.splice(torrentIndex, 1);
-        writeFileSync(this.magnetUriFile, JSON.stringify(magnetUris, null, 2));
+        magnetURIs.splice(torrentIndex, 1);
+        writeFileSync(this.magnetURIFile, JSON.stringify(magnetURIs, null, 2));
       }
     }
+  }
+}
+
+export class TorrentService {
+  constructor(downloadsFolder, magnetURIFile = "magnetURIs.json") {
+    this.client = new WebTorrent();
+    this.downloadsFolder = downloadsFolder;
+    this.uriFileManager = new MagnetURIFileManager(magnetURIFile);
+    this.loadTorrents();
+  }
+
+  addTorrent(magnetURI, downloadPath = "") {
+    const fullDownloadPath = path.join(this.downloadsFolder, downloadPath);
+
+    if (this.uriFileManager.torrentExists(magnetURI, fullDownloadPath)) {
+      throw new Error("Torrent já adicionado");
+    }
+    this.client.add(magnetURI, { path: fullDownloadPath });
+    this.uriFileManager.addTorrentOnFile(magnetURI, fullDownloadPath);
+  }
+
+  loadTorrents() {
+    const magnetURIs = this.uriFileManager.getMagnetURIs();
+
+    magnetURIs.forEach(({ magnetURI, downloadPath }) =>
+      this.client.add(magnetURI, { path: downloadPath })
+    );
   }
 
   async removeTorrent(infoHash) {
     const torrent = await this.client.get(infoHash);
 
-    this.removeTorrentFromFile(torrent.magnetURI);
-
     if (!torrent) {
       throw new Error("Torrent não encontrado");
     }
+
+    this.uriFileManager.removeTorrentFromFile(torrent.magnetURI);
 
     torrent.destroy((err) => {
       if (err) {

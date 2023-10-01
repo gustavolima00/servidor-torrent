cd4236516616bf583341d753bf3821ef17cfb831
Gustavo Marques <gustavomarques.gml@gmail.com>
2023-10-01 12:07:36 -0300

    Saving magnect uris

--- services/torrentService.mjs
+++ services/torrentService.mjs
@@ -1,21 +1,49 @@
 import WebTorrent from "webtorrent";
+import { readFileSync, writeFileSync, existsSync } from "fs";
 
 export class TorrentService {
-  constructor(downloadPath) {
+  constructor(downloadPath, magnetUriFile = "magnetUris.json") {
     this.client = new WebTorrent();
     this.downloadPath = downloadPath;
+    this.magnetUriFile = magnetUriFile;
+    this.loadTorrents();
   }
 
   addTorrent(magnetUri) {
-    return this.client.add(magnetUri, {
-      path: this.downloadPath,
-    });
+    this.client.add(magnetUri, { path: this.downloadPath });
+
+    let magnetUris = [];
+    if (existsSync(this.magnetUriFile)) {
+      magnetUris = JSON.parse(readFileSync(this.magnetUriFile, "utf-8"));
+    }
+
+    magnetUris.push(magnetUri);
+    writeFileSync(this.magnetUriFile, JSON.stringify(magnetUris, null, 2));
+  }
+
+  loadTorrents() {
+    if (existsSync(this.magnetUriFile)) {
+      const magnetUris = JSON.parse(readFileSync(this.magnetUriFile, "utf-8"));
+      magnetUris.forEach((magnetUri) => this.addTorrent(magnetUri));
+    }
+  }
+
+  removeTorrentFromFile(magnetURI) {
+    if (existsSync(this.magnetUriFile)) {
+      const magnetUris = JSON.parse(readFileSync(this.magnetUriFile, "utf-8"));
+      const torrentIndex = magnetUris.findIndex((uri) => magnetURI === uri);
+
+      if (torrentIndex > -1) {
+        magnetUris.splice(torrentIndex, 1);
+        writeFileSync(this.magnetUriFile, JSON.stringify(magnetUris, null, 2));
+      }
+    }
   }
 
   async removeTorrent(infoHash) {
     const torrent = await this.client.get(infoHash);
 
-    console.log("torrent", torrent);
+    this.removeTorrentFromFile(torrent.magnetURI);
 
     if (!torrent) {
       throw new Error("Torrent não encontrado");

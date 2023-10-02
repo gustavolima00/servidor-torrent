56a6185c8511514608a75f14e97057216522f81a
Gustavo Marques <gustavomarques.gml@gmail.com>
2023-10-02 09:53:26 -0300

    Fixing convert script

--- convert_captions.sh
+++ convert_captions.sh
@@ -6,5 +6,5 @@ find "$SOURCE_DIR" -type f -name '*.srt' | while read -r file; do
   OUTPUT_FILE="${file%.srt}.vtt"
   echo "Converting $file to $OUTPUT_FILE"
   ffmpeg -i "$file" "$OUTPUT_FILE"
-  # rm "$file"
+  rm "$file"
 done
--- convert_videos.sh
+++ convert_videos.sh
@@ -6,5 +6,5 @@ find "$SOURCE_DIR" -type f -name '*.mkv' | while read -r file; do
   OUTPUT_FILE="${file%.mkv}.mp4"
   echo "Converting $file to $OUTPUT_FILE"
   ffmpeg -i "$file" -codec:v libx264 -codec:a aac "$OUTPUT_FILE"
-  # rm "$file"
+  rm "$file"
 done
